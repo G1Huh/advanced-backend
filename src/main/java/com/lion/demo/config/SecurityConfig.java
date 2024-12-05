8e33ae51e1161044cc0e143178ff8a6c2a91e63b
G1Huh <hgw001468@naver.com>
2024-12-05 11:44:36 +0900

    security - 리팩토링 / socialLogin

--- src/main/java/com/lion/demo/config/SecurityConfig.java
+++ src/main/java/com/lion/demo/config/SecurityConfig.java
@@ -1,12 +1,20 @@
 package com.lion.demo.config;
 
+import com.lion.demo.security.JwtRequestFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 
 @Configuration
 public class SecurityConfig {
+    @Autowired
+    private AuthenticationFailureHandler failureHandler;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -27,6 +35,7 @@ public class SecurityConfig {
                         .usernameParameter("uid")
                         .passwordParameter("pwd")
                         .defaultSuccessUrl("/user/loginSuccess", true)  // 로그인 후 실행됨
+                        .failureHandler(failureHandler)
                         .permitAll()
                 )
                 .logout(auth -> auth
@@ -37,5 +46,17 @@ public class SecurityConfig {
                 );
         return http.build();
 
+    }
+
+    // JWT Filter Bean 등록
+    @Bean
+    public JwtRequestFilter jwtRequestFilter() {
+        return new JwtRequestFilter();
+    }
+
+    // Authentication Manager Bean 등록
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
